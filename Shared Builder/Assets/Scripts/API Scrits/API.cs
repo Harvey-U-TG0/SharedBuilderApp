cc5cd3d97351299a337820f77e5fc80fd591d351
Harvey-U-TG0 <64548399+Harvey-U-TG0@users.noreply.github.com>
2021-01-01 16:22:50 +0000

    Basic get request working with server

---
+++ Shared Builder/Assets/Scripts/API Scrits/API.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class API : MonoBehaviour
+{
+	// Call Method Through: StartCourotine(SendRequest());
+
+	private void Update()
+	{
+		if (Input.anyKeyDown)
+		{
+			StartCoroutine(SendRequest());
+			print("test");
+		}
+	}
+
+
+	IEnumerator SendRequest()
+	{
+		UnityWebRequest request = UnityWebRequest.Get("http://192.168.1.73:5000/getUser/Edward");
+		yield return request.SendWebRequest();
+
+		if (request.isNetworkError || request.isHttpError)
+		{
+			// Error Occurred
+			Debug.Log(request.error);
+		}
+
+		else
+		{
+			// Response can be accessed through: request.downloadHandler.text
+			Debug.Log(request.downloadHandler.text);
+		}
+	}
+}

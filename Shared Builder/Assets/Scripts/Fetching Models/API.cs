601d627461993d373bfc6fff434683fb3b32ac0b
Harvey-U-TG0 <64548399+Harvey-U-TG0@users.noreply.github.com>
2021-01-10 17:26:01 +0000

    User can cange their editing status and change their username

--- Shared Builder/Assets/Scripts/Fetching Models/API.cs
+++ Shared Builder/Assets/Scripts/Fetching Models/API.cs
@@ -8,6 +8,7 @@ public class API : MonoBehaviour
 	public Model latestModel;
 
 	public string currentModel = "test";
+	public string username = "Emily";
 
 	// after / is the model ID
 	//private string urlGetModel = "http://192.168.1.73:5000/getModel/test";
@@ -18,10 +19,31 @@ public class API : MonoBehaviour
 		currentModel = newID;
 	}
 
+	public void Setusername(string newName)
+	{
+		username = newName;
+	}
+
 	public void GetModel()
     {
 		StartCoroutine(SendModelRequest(currentModel));
     }
+
+	/// <summary>
+	/// Updates the users editing setting
+	/// </summary>
+	/// <param name="state">bool, False to stop editing, true to start editing</param>
+	public void UpdateEditing(bool newState)
+	{
+		if (newState == true)
+		{
+			StartCoroutine(SendEditStatus(currentModel));
+		}
+		else
+		{
+			StartCoroutine(SendEditStatus("False"));
+		}
+	}
 
 	IEnumerator SendModelRequest(string modelID)
 	{
@@ -48,5 +70,26 @@ public class API : MonoBehaviour
 		//models.Add(outputModel);
 	}
 
-	
+
+	IEnumerator SendEditStatus(string newEditstatus)
+	{
+		// Editing status is the new model we want to edit send string 'False'
+
+		UnityWebRequest request = UnityWebRequest.Get(baseURL +"updateUserEditing/" + username + "/" + newEditstatus);
+		yield return request.SendWebRequest();
+
+		if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+		{
+			// Error Occurred
+			Debug.Log(request.error);
+		}
+
+		else // The request is alis
+		{
+			// Response can be accessed through: request.downloadHandler.text
+			Debug.Log(request.downloadHandler.text);
+		}
+	}
+
+
 }

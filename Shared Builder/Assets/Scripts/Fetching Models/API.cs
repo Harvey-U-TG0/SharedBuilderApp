fd1e5307af0a7020e45a089f91f23d04ec029e8d
Harvey-U-TG0 <64548399+Harvey-U-TG0@users.noreply.github.com>
2021-01-10 16:42:37 +0000

    User can choose which model they want to edit

--- Shared Builder/Assets/Scripts/Fetching Models/API.cs
+++ Shared Builder/Assets/Scripts/Fetching Models/API.cs
@@ -7,19 +7,25 @@ public class API : MonoBehaviour
 {
 	public Model latestModel;
 
+	public string currentModel = "test";
 
 	// after / is the model ID
-	private string urlGetModel = "http://192.168.1.73:5000/getModel/test";
-
+	//private string urlGetModel = "http://192.168.1.73:5000/getModel/test";
+	private string baseURL = "http://192.168.1.73:5000/";
+	
+	public void SetModelId(string newID)
+	{
+		currentModel = newID;
+	}
 
 	public void GetModel()
     {
-		StartCoroutine(SendRequest(urlGetModel));
+		StartCoroutine(SendModelRequest(currentModel));
     }
 
-	IEnumerator SendRequest(string URL)
+	IEnumerator SendModelRequest(string modelID)
 	{
-		UnityWebRequest request = UnityWebRequest.Get(URL);
+		UnityWebRequest request = UnityWebRequest.Get(baseURL+"getModel/"+modelID);
 		yield return request.SendWebRequest();
 
 		if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
